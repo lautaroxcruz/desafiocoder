77b43901fd5b1b978f67dccee6e9d0a266aaeb32
lautaroxcruz <lautaroxcruz@gmail.com>
2022-07-28 10:48:52 -0300

    main

---
+++ script.js
@@ -0,0 +1,20 @@
+//bucles for
+
+let numeroIngresado = parseInt (prompt("Ingrese numero a multiplicar por dos hasta llegar a diez")) ; 
+for (let i=2; i <= 10 ; i++) {
+    let resultado = numeroIngresado * i;
+    console.log(numeroIngresado + " * " + i + "=" + resultado) ;
+}
+
+// condicionales 
+// Numero
+
+var culpable = window.prompt("¿eres culpable?"); // si, no
+
+	if(culpable=="si"){
+		document.write("irás a la cárcel");
+	}else if(culpable=="no"){
+		document.write("irás a casa");
+	}else{
+		document.write("la documentación por favor");
+	}
