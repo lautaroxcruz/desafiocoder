2ce32d770c47fb76b33a33399e3f812bda6e66bc
lautaroxcruz <lautaroxcruz@gmail.com>
2022-08-02 16:55:39 -0300

    main

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//ENTREGA 1 
 //bucles for
 
 let numeroIngresado = parseInt (prompt("Ingrese numero a multiplicar por dos hasta llegar a diez")) ; 
@@ -7,7 +8,7 @@ for (let i=2; i <= 10 ; i++) {
 }
 
 // condicionales 
-// Numero
+// numero
 
 var culpable = window.prompt("¿eres culpable?"); // si, no
 
@@ -19,4 +20,25 @@ var culpable = window.prompt("¿eres culpable?"); // si, no
 		document.write("la documentación por favor");
 	}
 
-	
+
+// ENTREGA 2
+
+let nota1, nota2, nota3;
+nota1 = prompt('Ingrese 1ra. nota:');
+nota2 = prompt('Ingrese 2da. nota:');
+nota3 = prompt('Ingrese 3ra. nota:');
+
+nota1 = parseInt(nota1);
+nota2 = parseInt(nota2);
+nota3 = parseInt(nota3);
+let pro;
+pro = (nota1 + nota2 + nota3) / 3;
+if (pro >= 7) {
+    document.write('¡Promocionaste!');
+} else {
+    if (pro >= 4) {
+        document.write('Regular');
+    } else {
+        document.write('Desaprobado');
+    }
+}
