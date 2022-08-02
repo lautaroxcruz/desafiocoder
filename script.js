7e4b41a1c5f5f1620da1b6fa2a79141f6e04eccf
lautaroxcruz <lautaroxcruz@gmail.com>
2022-08-02 16:57:16 -0300

    main

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 //ENTREGA 1 
 //bucles for
 
-let numeroIngresado = parseInt (prompt("Ingrese numero a multiplicar por dos hasta llegar a diez")) ; 
-for (let i=2; i <= 10 ; i++) {
-    let resultado = numeroIngresado * i;
-    console.log(numeroIngresado + " * " + i + "=" + resultado) ;
-}
+//let numeroIngresado = parseInt (prompt("Ingrese numero a multiplicar por dos hasta llegar a diez")) ; 
+//for (let i=2; i <= 10 ; i++) {
+ //   let resultado = numeroIngresado * i;
+  //  console.log(numeroIngresado + " * " + i + "=" + resultado) ;
+//}
 
 // condicionales 
 // numero
 
-var culpable = window.prompt("¿eres culpable?"); // si, no
+//var culpable = window.prompt("¿eres culpable?"); // si, no
 
-	if(culpable=="si"){
-		document.write("vas a la cárcel");
-	}else if(culpable=="no"){
-		document.write("vas a casa");
-	}else{
-		document.write("la documentación por favor");
-	}
+	//if(culpable=="si"){
+		//document.write("vas a la cárcel");
+	//}else if(culpable=="no"){
+	//	document.write("vas a casa");
+	//}else{
+		//document.write("la documentación por favor");
+//	}
 
 
 // ENTREGA 2
