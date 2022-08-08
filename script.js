033bda3cba54e61dbcb1108d5978674e67e1989b
lautaroxcruz <lautaroxcruz@gmail.com>
2022-08-08 19:13:57 -0300

    mian

--- script.js
+++ script.js
@@ -22,7 +22,6 @@
 
 
 // ENTREGA 2
-function
 let (nota1, nota2, nota3);
 alert("Ingresa tus notas");
 nota1 = prompt('Ingresa la 1ra. nota:');
@@ -58,48 +57,108 @@ calcular3cuotas(preciofinal);
 
 // Array en granos de cafe 
 
-let granosCafe = [
-  {
-    nombre: Perú,
-    ubicacion: "Villa Rica",
-    cantidad: "250G", 
-    tipoDeTostado: "Tueste claro", 
-    puntuacion: 86,
-    precioFinal: "$3.200",
+//productos declarados
+const Producto = [
+  { nombre: "Colombia", precio: 630 },
+  { nombre: "Peru", precio: 800 },
+  { nombre: "Guatemala", precio: 900 },
+  { nombre: "PAPÚA NUEVA GUINEA", precio: 620 },
+];
 
-  },
-
-  {
-    nombre: Colombia,
-    ubicacion: "Tolima",
-    cantidad: "250G", 
-    tipoDeTostado: "Tueste medio", 
-    puntuacion: 84,
-    precioFinal: "$2.700",
-  },
-{
-  nombre: Guatemala,
-  ubicacion: "HUEHUETENANGO",
-  cantidad: "250G",
-  tipoDeTostado: "Tueste claro",
-  puntuacion: 86,
-  precioFinal: "$3.200",
-},
-
-{
-  nombre: PAPÚANUEVAGUINEA,
-  ubicacion: "WAHGI VALLEY",
-  cantidad: "250G",
-  tipoDeTostado: "TUESTE CLARO",
-  puntuacion: 86.50,
-  precioFinal: "$3.900",
+//clase constructora de nuevo objetos
+class cafe {
+  constructor(nombre, precio) {
+      this.nombre = nombre
+      this.precio = precio
+  }
 }
-]
 
-let compra=parseInt(prompt("¡Hola! ¿te gustaría adquirir alguna variedad de cafe? \ningrese el numero del producto que desee \n1-Perú\n2-Colombia\n3-Guatemala\n4-PAPÚANUEVAGUINEA\n5-Solo observo :)"));
-if(compra<4){
-    alert("el valor a pagar es de $"+granosCafe[compra-1].precioFinal)
+// funcion para agregar nuevas variedades de cafe
+function agregarcafe() {
+  let nombreCafe = prompt("ingrese el nombre del cafe")
+  let nuevoPrecio = prompt("ingrese el precio")
+
+
+  let cafenuevo = new cafe(nombreCafe, nuevoPrecio,)
+
+  Producto.push(cafenuevo)
+  console.log(Producto)
+  return null;
+
+}
+
+//Carrito
+let carrito = [];
+
+//funcion para agregar al carro
+const addCarrito = () => {
+  let nombre = prompt("ingrese una variedad")
+  let resultado = Producto.find((n) => n.nombre.toLowerCase() === nombre.toLowerCase());
+
+
+  if (resultado) {
+      carrito.push(resultado);
+      console.log(carrito);
+  } else {
+      alert("Esta variedad no esta disponible");
+  }
 };
 
+//funcion precio final
+function precioFinal() {
+  let totalPrecios = carrito.reduce(((acumulador, carrito) => acumulador + carrito.precio), 0);
+  return totalPrecios;
+}
 
-  
+//interacion con el usuario
+let proceso = true;
+do {
+  let intUsuario = prompt("Desea comprar(Si/No) o Agregar un producto a stock(ADD)").toLowerCase()
+
+  while (intUsuario != "si" && intUsuario != "no" && intUsuario != "add") {
+      alert("Por favor ingrese una opcion")
+      intUsuario = prompt("Desea comprar(Si/No) o Agregar un producto(ADD)")
+  }
+  //Si el usuario quiere comprar, agrega al carrito y devuelve precio final
+  if (intUsuario == "si") {
+      let continuar = true;
+      do {
+          alert("Nuestra lista de productos")
+          let todosLosProductos = Producto.map((Producto) => Producto.nombre + " $" + Producto.precio);
+          alert(todosLosProductos.join(" - "));
+          addCarrito()
+          let afirmativo = prompt("Desea continuar?(Si/No)").toUpperCase()
+          if (afirmativo == "NO") {
+              continuar = false;
+
+          }
+      } while (continuar == true)
+      alert("Precio final es: " + precioFinal())
+  }
+
+  else if (intUsuario == "add") {
+      let continuar = true;
+
+      //agregar variedad de cafe a productos
+      do {
+          agregarlibro();
+          alert("Libro agregados")
+          let afirmativo = prompt("Desea agregar otro libro?(Si/No)").toUpperCase();
+          if (afirmativo == "NO") {
+              continuar = false;
+
+          }
+      } while (continuar == true)
+      alert("Libro/s agregados");
+      let seguir = prompt("Desea continuar en la pagina?(Si/No)").toUpperCase
+      if (seguir == "NO") {
+          proceso = false;
+      }
+  }
+  else if (intUsuario == "no") {
+      proceso = false;
+  }
+
+} while (proceso == true);
+
+alert("¡Hasta luego!")
