6a5bf223b2bb8769c2b92a06c608add75a58e442
lautaroxcruz <lautaroxcruz@gmail.com>
2022-08-08 20:17:59 -0300

    main

---
+++ desafio1/script.js
@@ -0,0 +1,21 @@
+//ENTREGA 1 
+//bucles for
+
+let numeroIngresado = parseInt (prompt("Ingrese numero a multiplicar por dos hasta llegar a diez")) ; 
+for (let i=2; i <= 10 ; i++) {
+    let resultado = numeroIngresado * i;
+    console.log(numeroIngresado + " * " + i + "=" + resultado) ;
+}
+
+condicionales 
+ numero
+
+var culpable = window.prompt("¿eres culpable?"); // si, no
+
+	if(culpable=="si"){
+		//document.write("vas a la cárcel");
+	}else if(culpable=="no"){
+		document.write("vas a casa");
+	}else{
+		//document.write("la documentación por favor");
+	}
